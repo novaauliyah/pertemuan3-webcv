1ba88da7c82a2a6a32a878f219bc1802d27c1031
“Pemula <auliyahnofa24@gmail.com>
2023-04-28 22:54:57 +0700

    revisi

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-#from flask_ngrok import run_with_ngrok
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
 
@@ -10,5 +10,5 @@ def halamanCV():
 
 
     if __name__== "__main__":
-        #run_with_ngrok(app)
+        run_with_ngrok(app)
         app.run()
